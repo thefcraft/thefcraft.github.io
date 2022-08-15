81df4bd2e5bbaf4d13d02c747e29e24e451c3888
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2022-08-15 18:35:17 +0530

    Add files via upload

---
+++ js/add.project.js
@@ -0,0 +1,10 @@
+const myElement = document.getElementById("add.project");
+function addProject(title, description, image, link){
+    myElement.innerHTML += '<div class="col-md-6 col-sm-6"><!-- PROJECT ITEM --><div class="project-item"><a href="'+
+    link+'" class="image-popup"><img src="'+image+
+    '" class="img-responsive" alt=""><div class="project-overlay"><div class="project-info"><h1>'+
+    title+'</h1><h3>'+description+'</h3></div></div></a></div></div> '
+}
+
+// addProject('images/project-image4.jpg', 'title', 'description')
+addProject('data cryptor', 'encrypt or decrypt text', 'images/data-cryptor.png', 'https://thefcraft.github.io/data-cryptor-v1.0/')
